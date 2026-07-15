c845d7d02afd741184bc88fbf336c2b4e87bc0cf
navkiratsingh90 <kiratsingh2181@gmail.com>
2026-07-15 23:41:06 +0530

    feat : final commit

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 //process.env.NEXT_PUBLIC_BACKEND_URL 
 
 const api = axios.create({
